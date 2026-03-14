cb5f36d35f042df2eb51af081f23c1eab51051ab
ahnirob4 <ahnirob4@gmail.com>
2026-03-14 06:25:38 +0300

    String added here

---
+++ test.py
@@ -0,0 +1,20 @@
+
+# a,b,c = map(int,input().split())
+
+# max = a
+# min = a
+
+
+# for i in [b,c]:
+#     if i > max:
+#         max = i
+#     if i < min:
+#         min = i
+
+# print(min, max)
+    
+# ______________________________________________________________
+
+n = int(input())
+number = list(map(int, input(n).split()))
+print(number)
